7216e67d7c92fc0f1933eefc1429cc390b64ebb8
Joel Lee <113893251+Joekovich@users.noreply.github.com>
2024-03-04 15:39:03 +0100

    Add files via upload

---
+++ you.js
@@ -0,0 +1,10 @@
+const menuIcon = document.getElementById(".menuIcon");
+const container = document.getElementById(".container");
+
+ dropDown.addEventListener("click", () => {
+     container.classList.toggle("");
+});
+
+const dropdown = () => document.body.classList.toggle('Hamburger');
+
+//console.log(menuIcon);
